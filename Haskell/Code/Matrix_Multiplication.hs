cdcc715825ec36211826a233c04b80dd089c9bab
Dan Lemberg <111188654+lembergdan@users.noreply.github.com>
2024-03-24 14:11:23 +0200

    Update Matrix_Multiplication.hs

--- Haskell/Code/Matrix_Multiplication.hs
+++ Haskell/Code/Matrix_Multiplication.hs
@@ -1,3 +1,13 @@
+import Control.Monad.ST
+import Data.Array.ST 
+import Control.Monad 
+import Data.STRef
+import Data.Array.Unboxed
+import Data.Time.Clock
+import Numeric.LinearAlgebra hiding ((!))
+import Control.Parallel
+import Control.Parallel.Strategies
+
 mult :: Int -> Int -> Int -> UArray Int Double -> UArray Int Double -> UArray Int Double 
 mult m k n a b = runSTUArray $ do 
     arr <- newArray (0, m * n - 1) 0
@@ -15,3 +25,21 @@ mult m k n a b = runSTUArray $ do
         dot :: Int -> Int -> Int -> Double -> Double
         dot i j t acc | t == k    = acc
                       | otherwise = dot i j (t + 1) (acc + (getA i t * getB t j))
+
+n = 1000
+
+main = do 
+    arr1 <- rand n n 
+    arr2 <- rand n n 
+
+    let !l1 = concat $ toLists arr1 
+    let !l2 = concat $ toLists arr2
+
+    let !a = listArray (0, n * n - 1) l1
+    let !b = listArray (0, n * n - 1) l2
+
+    tt1 <- getCurrentTime 
+    let mm = mult n n n a b 
+    print $ mm ! 2     
+    tt2 <- getCurrentTime
+    print (diffUTCTime tt2 tt1)
